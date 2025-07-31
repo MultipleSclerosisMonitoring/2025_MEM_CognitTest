8d00f3b004ffacf47223c03ae89f41447cd57849
miguelminue <m.minue@alumnos.upm.es>
2025-07-31 17:51:53 +0200

    SPhinx doc uploaded

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '1.1.4'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'myst_parser',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,3 +28,8 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+source_suffiX = {
+    '.rst': 'restrcturedtext',
+    '.md': 'markdown'
+}
